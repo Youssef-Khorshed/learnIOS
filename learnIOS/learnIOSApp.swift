dde5d4e93943848a18555e4aa29aeb33fdc8d827
Youssef-Khorshed <youssefmohamedeslayed2000@gmail.com>
2024-11-12 12:29:33 +0200

    Initial Commit

---
+++ learnIOS/learnIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  learnIOSApp.swift
+//  learnIOS
+//
+//  Created by youssef on 12/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct learnIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
